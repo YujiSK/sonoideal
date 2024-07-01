c3f29cb4642ae728881d50f3f3cad5458f32f898
YujiSK <yujikavano@gmail.com>
2024-07-01 14:40:03 +0900

    2024_07_01

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,35 @@ use App\Http\Controllers\LineWebhookController;
 
 require __DIR__.'/api.php';
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+Route::get('/newlogin', function () {
+    return view('newlogin');
+})->name('newlogin');
+Route::get('/login-2', function () {
+    return view('login-2');
+})->name('login-2');
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+Route::get('/manualcontrol', function () {
+    return view('manualcontrol');
+})->name('manualcontrol');
+Route::get('/setting', function () {
+    return view('setting');
+})->name('setting');
+Route::get('/sensorcontrol', function () {
+    return view('sensorcontrol');
+})->name('sensorcontrol');
+Route::get('/Remotecontrol', function () {
+    return view('Remotecontrol');
+})->name('Remotecontrol');
+Route::get('/Remotecontrol-2', function () {
+    return view('Remotecontrol-2');
+})->name('Remotecontrol-2');
+Route::get('/Remotecontrol-3', function () {
+    return view('Remotecontrol-3');
+})->name('Remotecontrol-3');
 Route::get('raspi/sensor/index', [SensorController::class, 'index']);
 Route::post('/line/webhook', [LineWebhookController::class, 'handle']);
