33654f50d1e22d55cacb5a2c66c3ec2862b24a05
YujiSK <yujikavano@gmail.com>
2024-07-01 18:18:14 +0900

    Format

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -8,18 +8,18 @@ use App\Models\Sensor;
 class SensorController extends Controller
 {
     public function index()
-	{
-		$sensor_data = Sensor::orderBy('created_at', 'desc')->take(10)->get();
-    		return view('raspi.sensor.index', ['sensor_data' => $sensor_data]);
+    {
+        $sensor_data = Sensor::orderBy('created_at', 'desc')->take(10)->get();
+        return view('raspi.sensor.index', ['sensor_data' => $sensor_data]);
     }
 
-	public function store(Request $request)
-	{
-        	$sensorData = new Sensor;
-        	$sensorData->temperature = $request->temperature;
-        	$sensorData->humidity = $request->humidity;
-        	$sensorData->save();
+    public function store(Request $request)
+    {
+        $sensorData = new Sensor;
+        $sensorData->temperature = $request->temperature;
+        $sensorData->humidity = $request->humidity;
+        $sensorData->save();
 
-        	return response()->json(['message' => 'Data saved successfully'], 200);
-	}
+        return response()->json(['message' => 'Data saved successfully'], 200);
+    }
 }
